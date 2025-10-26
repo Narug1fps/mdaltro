380fd5b3e432705e271cf961936cc22b348fe35c
Narug1fps <144721920+Narug1fps@users.noreply.github.com>
2025-10-26 17:00:58 -0300

    Replace Facebook Pixel with Google Analytics script
    Removed Facebook Pixel integration and added Google Analytics script.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
-import { FB_PIXEL_ID } from "@/lib/fpixel";
 import Script from "next/script";
 
 export const metadata: Metadata = {
@@ -20,21 +19,21 @@ export default function RootLayout({
   return (
     <html lang="pt-br" className='selection:bg-white bg-black'>
       <head>
+       <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        />
         <Script
-          id="fb-pixel"
+          id="gtag-init"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '${FB_PIXEL_ID}');
-              fbq('track', 'PageView');
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+                page_path: window.location.pathname,
+              });
             `,
           }}
         />
