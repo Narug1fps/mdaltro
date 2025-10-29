47baf6d5d1c288a4c79bc970021c91fce697d420
Narug1fps <144721920+Narug1fps@users.noreply.github.com>
2025-10-29 20:06:51 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,34 +2,32 @@ import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import { Analytics } from '@vercel/analytics/next'
+import Script from 'next/script'
 import './globals.css'
-import Script from "next/script";
 
 export const metadata: Metadata = {
   title: 'Marcelo Daltro',
-  description: 'Clinica Marcelo Daltro',
-  
+  description: 'Clínica Marcelo Daltro',
 }
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
-}>)
-  
+}>) {
   const GA_ID = process.env.NEXT_PUBLIC_GTAG_ID
-{
+
   return (
-    <html lang="pt-br" className='selection:bg-white bg-black'>
+    <html lang="pt-br" className="selection:bg-white bg-black">
       <head>
-     {GA_ID && (
+        {GA_ID && (
           <>
             <Script
               strategy="afterInteractive"
               src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
             />
             <Script
-              id="google-ads-pixel"
+              id="google-ads-base"
               strategy="afterInteractive"
               dangerouslySetInnerHTML={{
                 __html: `
@@ -42,11 +40,10 @@ export default function RootLayout({
             />
           </>
         )}
-      <link rel="shortcut icon" href="/logo.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="/logo.ico" type="image/x-icon" />
       </head>
-      <body  className={`font-sans ${GeistSans.variable} ${GeistMono.variable} `}>
-          
-          {children}
+      <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
+        {children}
         <Analytics />
       </body>
     </html>
